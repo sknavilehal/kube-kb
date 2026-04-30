8805434c4dd155ab4c80db74cca0c5fb1dedda7d
Shamanth N R (shamnr) <shamnr@cisco.com>
2026-05-01 01:31:34 +0530

    operator scaffolding

---
+++ api/v1alpha1/knowledgebase_types.go
@@ -0,0 +1,64 @@
+/*
+Copyright 2026.
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+*/
+
+package v1alpha1
+
+import (
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+)
+
+// EDIT THIS FILE!  THIS IS SCAFFOLDING FOR YOU TO OWN!
+// NOTE: json tags are required.  Any new fields you add must have json tags for the fields to be serialized.
+
+// KnowledgeBaseSpec defines the desired state of KnowledgeBase
+type KnowledgeBaseSpec struct {
+	// INSERT ADDITIONAL SPEC FIELDS - desired state of cluster
+	// Important: Run "make" to regenerate code after modifying this file
+
+	// Foo is an example field of KnowledgeBase. Edit knowledgebase_types.go to remove/update
+	Foo string `json:"foo,omitempty"`
+}
+
+// KnowledgeBaseStatus defines the observed state of KnowledgeBase
+type KnowledgeBaseStatus struct {
+	// INSERT ADDITIONAL STATUS FIELD - define observed state of cluster
+	// Important: Run "make" to regenerate code after modifying this file
+}
+
+// +kubebuilder:object:root=true
+// +kubebuilder:subresource:status
+
+// KnowledgeBase is the Schema for the knowledgebases API
+type KnowledgeBase struct {
+	metav1.TypeMeta   `json:",inline"`
+	metav1.ObjectMeta `json:"metadata,omitempty"`
+
+	Spec   KnowledgeBaseSpec   `json:"spec,omitempty"`
+	Status KnowledgeBaseStatus `json:"status,omitempty"`
+}
+
+// +kubebuilder:object:root=true
+
+// KnowledgeBaseList contains a list of KnowledgeBase
+type KnowledgeBaseList struct {
+	metav1.TypeMeta `json:",inline"`
+	metav1.ListMeta `json:"metadata,omitempty"`
+	Items           []KnowledgeBase `json:"items"`
+}
+
+func init() {
+	SchemeBuilder.Register(&KnowledgeBase{}, &KnowledgeBaseList{})
+}
